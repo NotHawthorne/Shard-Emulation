f636fd2f66685ee9b0b9c6b43a71b06259bf71ee
Kaev <Demonhunter@hotmail.de>
2014-11-02 09:29:46 +0100

    fixed capital letter, which caused a LUA error

--- general/login.lua
+++ general/login.lua
@@ -5,8 +5,8 @@ local function languagefix (event, player)
 	player:LearnSpell(668, player)
 end
 
-local function register_stats (event, Player)
-	CharDBExecute("INSERT INTO shard_aa_points (playerguid) VALUES ("..Player:GetGUIDLow()..")")
+local function register_stats (event, player)
+	CharDBExecute("INSERT INTO shard_aa_points (playerguid) VALUES ("..player:GetGUIDLow()..")")
 	base_statdb = WorldDBQuery("SELECT str, agi, sta, inte, spi FROM player_levelstats WHERE race="..player:GetRace().."")
 	CharDBExecute("INSERT INTO shard_stats (playerguid, str, agi, sta, inte, spi) VALUES ("..base_statdb:GetUInt32(0)..", "..base_statdb:GetUInt32(1)..", "..base_statdb:GetUInt32(2)..", "..base_statdb:GetUInt32(3)..", "..base_statdb:GetUInt32(4)..")")
 end
